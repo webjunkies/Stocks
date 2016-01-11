9f682ccc01675828c7c8ef22d81bc774e2d7f9f2
Jake Cox <nicodangelo15@gmail.com>
2016-01-10 22:09:20 -0600

    ok I am finally done
    I finished the math sequence that will add and subtract when you buy
    things in the game(completely temporary)

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ var amount = 0, amountFactor = 1;
 
 //functions
 function click(){
-    document.getElementById('amount').innerHTML = amount;
-    document.getElementById('add-amount').innerHTML = amountFactor;
+    document.getElementById('amount-display').innerHTML = amount;
+    document.getElementById('add-amount').innerHTML = amountFactor + 1 + " cost: " + ((amountFactor + 1) + (amountFactor + 1));
 };
 
 function add(){
     amount = amount + amountFactor;
     document.getElementById('amount-display').innerHTML = amount;
-}
+    document.getElementById('add-amount').innerHTML = amountFactor + 1 + " cost: " + ((amountFactor + 1) + (amountFactor + 1));
+};
 
 function addAmount(){
-    amountFactor = amountFactor + 1;
-    document.getElementById('add-amount').innerHTML = amountFactor + 1;
-}
+    if(amount >= (amountFactor + 1) + (amountFactor + 1)){
+        amount -= (amountFactor + 1) + (amountFactor + 1);
+        amountFactor = amountFactor + 1;
+    }
+    document.getElementById('amount-display').innerHTML = amount;
+    document.getElementById('add-amount').innerHTML = amountFactor + 1 + " cost: " + ((amountFactor + 1) + (amountFactor + 1));
+};
