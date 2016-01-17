2417911525cab3314a88712e90c41c977fa443d9
Jake Cox <nicodangelo15@gmail.com>
2016-01-17 17:46:36 -0600

    This is a good point
    I have a button that does nothing. their is a lot of stuff that is
    commented and there is some text at the top of the screen that will add
    one number to itself every single second no matter what type of computer
    you have!

--- button.js
+++ button.js
@@ -1,30 +1,57 @@
-function Button(x, y, width, height, text){
+function Button(x, y, width, height, text, error){
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.text = text;
     this.color = "#FFA500";
+    this.error = error;
     
-    this.setup = function(context){
-        
-    }
+    //first thing called to create the button object
+    this.setup = function(context){}
     
-    this.update = function(context){
-        
-    }
+    //the update function for the button object
+    this.update = function(context){}
     
+    //the render function for the button object
     this.render = function(context){
+        //sets the original fill color
         context.fillStyle = this.color;
+        //render the button
         context.fillRect(this.x, this.y, this.width, this.height);
+        
+        //if you want to display the text
+        if(this.text != null)
+        {
+            //text options
+            var fontSize = 40;
+            context.fillStyle = "white";
+            context.font = fontSize + "px sans-serif";
+            
+            //text position
+            var textSize = context.measureText(this.text);
+            var textX = this.x + (this.width/2) - (textSize.width / 2);
+            var textY = this.y + (this.height/2) + error;
+            
+            //render the text 
+            context.fillText(this.text, textX, textY);
+            context.fillStyle = this.color;
+        }
     }
     
-    this.event = function(mousex, mousey){
+    //When the user presses down change the buttons color to signal the user what they clicked
+    this.eventMouseDown = function(mousex, mousey, game){
+        //Checks if the click was inside this button
         if(mousex >= this.x && mousex <= (this.x + this.width)
             && mousey >= this.y && mousey <= (this.y + this.height))
         {
-            this.color = "#000000";
+            this.color = "#ff8333";
         }
+    }
+        
+    //When the user releses their mouse button set the button to its original color
+    this.eventMouseUp = function(mousex, mousey, game){
+            this.color = "#FFA500";
         
     }
 }
---
+++ game.js
@@ -0,0 +1,71 @@
+function Game(canvas, context)
+{
+    //Instance variables
+    this.error = error;
+    var button = null;
+    var amount = 0;
+    
+    //The first thing that should get called
+    this.setup = function(canvas, context)
+    {
+        //Create all the Items on the screen
+        button = new Button(0, 300, 300, 80, "Button", this.error);
+        
+        //Check for the mouse events
+        canvas.addEventListener("mousedown", eventMouseDown);
+        canvas.addEventListener("mouseup", eventMouseUp);
+    }
+    
+    //Runs the "Game loop"
+    this.run = function()
+    {
+        //calls update every one seccond
+        setInterval(this.update, 1000);
+        //calls render 60 times a seccond
+        setInterval(this.render, 1000 / 60);
+    }
+    
+    //updates all the game mechanics and numbers
+    this.update = function()
+    {
+        button.update();
+        amount++;
+    }
+    
+    //renders the screen(60 times a seccond)
+    //or said as 60 frames per seccond
+    this.render = function()
+    {
+        //"clears" the screen for the next frame
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        
+        //call all render functions here
+        button.render(context);
+        context.fillStyle = "#000000";
+        context.font="40px Georgia";
+        context.fillText(amount, 30, 30);
+    }
+    
+    //called from the mouse being clicked
+    eventMouseDown = function(e)
+    {
+        mousex = e.pageX - error;
+        mousey = e.pageY - error;
+        // just shows you where the mouse click is in the console
+        console.log("x: " + e.clientX + " Y: " + e.clientY);
+        
+        //call all objects that need a mouse event down
+        button.eventMouseDown(mousex, mousey, this);
+    }
+    
+    //called from the mouse being released
+    eventMouseUp = function(e)
+    {
+        mousex = e.pageX - error;
+        mousey = e.pageY - error;
+        console.log("x: " + e.clientX + " Y: " + e.clientY);
+        
+        //call all objects that need a mouse event up
+        button.eventMouseUp(mousex, mousey, this);
+    }
+}
